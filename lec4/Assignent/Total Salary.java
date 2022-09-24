fda8126e8d67c553e84745d9c3cf5958d350ce1c
Bluebird0223 <70681471+Bluebird0223@users.noreply.github.com>
2022-09-24 19:53:49 +0530

    Update Total Salary.java

--- lec4/Assignent/Total Salary.java
+++ lec4/Assignent/Total Salary.java
@@ -1,30 +1,25 @@
-import java.util.Scanner;
-public class Solution {
-
-
+public class Main {
+	
 	public static void main(String[] args) {
-		
-		/* Your class should be named Solution.
-	 	* Read input as specified in the question.
-	 	* Print output as specified in the question.
-		*/
-        int s,e,w;
-        int i;
-        Scanner g=new Scanner(System.in);
-        s=g.nextInt();
-        e=g.nextInt();
-        w=g.nextInt();
-        while(s<=e){
-             
-            // s=s+w;
-            // System.out.println(w+" "+i);
-            i=(s-32)*5/9;
-            System.out.println(s+" "+i);
-            s=s+w;
+		// Write your code here
+        int allow;
+        Scanner s=new Scanner(System.in);
+        int basic=s.nextInt();
+        Character c=s.next().charAt(0);
+        int n=c;
+        if(n==65){
+            allow=1700;
+        }else if(n==66){
+            allow=1500;
+        }else {
+            allow=1300;
         }
         
+        double Total_salary=((basic+(0.20*basic)+(0.50*basic)+allow)-(0.11*basic));
+        double p=Math.round(Total_salary);
+        int h=(int)(p);
+        System.out.println(h);
 
-		
+
 	}
-
 }
