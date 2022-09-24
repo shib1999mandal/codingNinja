13d2fc47e49c5e0296cf2a215780fa9dc577ed17
Bluebird0223 <70681471+Bluebird0223@users.noreply.github.com>
2022-09-24 18:21:37 +0530

    Create Find Character Case.cpp

---
+++ lec4/Find Character Case.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	// Write your code here
+    char c;
+    cin>>c;
+    int n=c;
+        if(n>=65&&n<=99)
+        {
+            cout<<"1";
+            //System.out.println("1");
+        }else if(n>=97&&n<=122){
+            cout<<"0";
+            //System.out.println("0");
+        }else
+        cout<<"-1";
+        //System.out.println("-1");
+	
+}
