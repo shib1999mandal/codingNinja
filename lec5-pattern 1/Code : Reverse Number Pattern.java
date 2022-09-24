4c1f3c5505e8db80e034ba7b9056f8ec6d6fa432
Bluebird0223 <70681471+Bluebird0223@users.noreply.github.com>
2022-09-24 18:31:16 +0530

    Create Code : Reverse Number Pattern.java

---
+++ lec5-pattern 1/Code : Reverse Number Pattern.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+         Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int i=0;
+        while(i<=n){
+            int j=i;
+            while(j>=1){
+                System.out.print(j);
+                j--;     
+            }
+            System.out.println();
+            i++;
+        }
+		
+	}
+
+}
