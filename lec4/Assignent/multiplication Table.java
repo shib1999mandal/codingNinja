954889ce8d096259f88bc2be0b6410bfc8fb6217
Bluebird0223 <70681471+Bluebird0223@users.noreply.github.com>
2022-09-24 18:25:36 +0530

    Create multiplication Table.java

---
+++ lec4/Assignent/multiplication Table.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+	
+	public static void main(String[] args) {
+		// Write your code here
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt(),i=1;
+        while(i<=10){
+            System.out.println(i*n);
+            i++;
+        }
+
+	}
+}
