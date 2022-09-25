eeb060419e8d822a3e9fdf1a8c062ae4f9f601b2
Bluebird0223 <70681471+Bluebird0223@users.noreply.github.com>
2022-09-25 13:08:00 +0530

    Create Pair Sum.cpp

---
+++ lec9 Arrays/Assignment/Pair Sum.cpp
@@ -0,0 +1,15 @@
+int pairSum(int *arr, int size, int x)
+{
+	//Write your code here
+     int count = 0; // Initialize result
+ 
+    // Consider all possible pairs and check their sums
+    for (int i = 0; i < size; i++){
+        for (int j = i + 1; j < size; j++){
+            if (arr[i] + arr[j] == x)
+                count++;
+        }
+    }
+ 
+    return count;
+}
