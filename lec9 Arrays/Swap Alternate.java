6cbbe6f0022ee56acdb495b6862f6c64ff6114df
Bluebird0223 <70681471+Bluebird0223@users.noreply.github.com>
2022-09-25 13:06:06 +0530

    Create Swap Alternate.java

---
+++ lec9 Arrays/Swap Alternate.java
@@ -0,0 +1,13 @@
+
+public class Solution {
+    
+    public static void swapAlternate(int arr[]) {
+    	//Your code goes here
+        int temp=0,n=arr.length;
+        for(int i=0;i<n-1;i+=2){
+         temp=arr[i];
+         arr[i]=arr[i+1];
+         arr[i+1]=temp;
+         }
+    }
+}
