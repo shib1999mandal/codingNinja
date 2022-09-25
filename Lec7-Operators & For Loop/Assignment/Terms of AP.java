8477420ad609818874778d03c0eedd3a31dc7383
Bluebird0223 <70681471+Bluebird0223@users.noreply.github.com>
2022-09-25 12:33:06 +0530

    Create Terms of AP.java

---
+++ Lec7-Operators & For Loop/Assignment/Terms of AP.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main {
+	
+	public static void main(String[] args) {
+		// Write your code here
+        Scanner s=new Scanner(System.in);
+        int n = s.nextInt();
+    	int count = 0;
+    for(int i=1; count<n;i++)
+    {
+    int ap = 3*i+2;
+    if(ap%4!=0)
+    {
+    System.out.print(ap+" ");
+    count++;
+    }
+	}
+
+	}
+}
