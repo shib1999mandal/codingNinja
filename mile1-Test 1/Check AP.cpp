9306e7b948919537f6e3b831fec1688df857aa89
shib1999mandal <66518902+shib1999mandal@users.noreply.github.com>
2022-09-29 02:33:15 +0530

    Update Check AP.cpp

--- mile1-Test 1/Check AP.cpp
+++ mile1-Test 1/Check AP.cpp
@@ -1,3 +1,4 @@
+// using c++  ::
 #include<bits/stdc++.h>
 using namespace std;
 bool checkIsAP(int arr[], int n)
@@ -26,4 +27,41 @@ for(int i=0;i<n;i++){
 					(cout << "false" << endl);
 
 return 0;
+}
+
+
+
+// try to write the same code in  Java:
+
+
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int ar[] = new int[n];
+        for(int i=0; i<n; i++){
+            ar[i] = sc.nextInt();
+			
+        }
+
+		boolean ap = true;
+
+		int d = ar[1] - ar[0]; 
+        for(int i=0; i<n-1; i++){
+			if(ar[i+1] - ar[i] != d){
+				ap = false;
+
+				break;
+
+			}
+
+		}
+			if(ap)
+				System.out.println(true);
+				else
+					System.out.println(false);
+
+		}
 }
